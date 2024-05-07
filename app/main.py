1bb4561b17ff3ff1ad3f2ecc2aef200499d43121
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-05-07 22:44:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -10,13 +10,19 @@ def calculate_profit(trade: str) -> None:
         trades = json.load(file)
     for trade in trades:
         if trade["bought"]:
-            plus_coin = str(Decimal(matecoin_account) + Decimal(trade["bought"]))
-            minus_money = str(Decimal(earned_money) - Decimal(trade["matecoin_price"]) * Decimal(trade["bought"]))
+            plus_coin = str(Decimal(matecoin_account)
+                            + Decimal(trade["bought"]))
+            minus_money = str(Decimal(earned_money)
+                              - Decimal(trade["matecoin_price"])
+                              * Decimal(trade["bought"]))
             matecoin_account = plus_coin
             earned_money = minus_money
         if trade["sold"]:
-            plus_money = str(Decimal(earned_money) + Decimal(trade["matecoin_price"]) * Decimal(trade["sold"]))
-            minus_coin = str(Decimal(matecoin_account) - Decimal(trade["sold"]))
+            plus_money = str(Decimal(earned_money)
+                             + Decimal(trade["matecoin_price"])
+                             * Decimal(trade["sold"]))
+            minus_coin = str(Decimal(matecoin_account)
+                             - Decimal(trade["sold"]))
             matecoin_account = minus_coin
             earned_money = plus_money
     profit["earned_money"] = earned_money
